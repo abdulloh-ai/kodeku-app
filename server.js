9892ddd7db43cfa7ff6a79c5a29671cd123faaee
Hanif Abdulloh <hanifabdullohhanifabdulloh@gmail.com>
2026-08-14 16:24:28 +0700

    Feat: Add visual 8-Kelas Master Roadmap Framework directly to lesson body and sidebar with cache-control headers

--- server.js
+++ server.js
@@ -219,7 +219,12 @@ const server = http.createServer((req, res) => {
         res.end('Check site admin for error: ' + error.code + ' ..\n');
       }
     } else {
-      res.writeHead(200, { 'Content-Type': contentType });
+      res.writeHead(200, {
+        'Content-Type': contentType,
+        'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0',
+        'Pragma': 'no-cache',
+        'Expires': '0'
+      });
       res.end(content, 'utf-8');
     }
   });
